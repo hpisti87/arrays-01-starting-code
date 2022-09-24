c23bb8629e7d2adcf2ce036e7107e319bc824e83
hpisti87 <pisti87@gmail.com>
2022-09-24 17:43:26 +0200

    Section 8: 187

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ console.log(listItems);
 
 const arrayListItems = Array.from(listItems);
 console.log(arrayListItems);
+
+const analyticsData = [
+  [1, 1.6],
+  [-5.4, 2.1]
+];
+
+for (const data of analyticsData) {
+  for (const dataPoint of data) {
+    console.log(dataPoint);
+  }
+}
