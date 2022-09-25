df516a4745ebf4ad296954d59acacbb01cde062d
hpisti87 <pisti87@gmail.com>
2022-09-25 10:26:28 +0200

    Section 8: 188

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
 // const moreNumber = new Array(5);
 // console.log(moreNumber);
@@ -7,19 +7,30 @@ console.log(numbers);
 // const yetMoreNumbers = Array.of(1, 2);
 // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
 
-const analyticsData = [
-  [1, 1.6],
-  [-5.4, 2.1]
-];
+// const analyticsData = [
+//   [1, 1.6],
+//   [-5.4, 2.1]
+// ];
 
-for (const data of analyticsData) {
-  for (const dataPoint of data) {
-    console.log(dataPoint);
-  }
-}
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
+
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Reading');
+hobbies.unshift('Coding');
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+hobbies[5] = 'Reading';
+console.log(hobbies, hobbies[3]);
